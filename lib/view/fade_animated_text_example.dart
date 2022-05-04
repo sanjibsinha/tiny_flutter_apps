faf140adc88cf073995ef2e8fa60d554e5d2d31a
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-04 12:49:10 +0530

    faded animated navigation

---
+++ lib/view/fade_animated_text_example.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FadeAnimatedTextExample extends StatefulWidget {
+  const FadeAnimatedTextExample({Key? key}) : super(key: key);
+
+  @override
+  State<FadeAnimatedTextExample> createState() =>
+      _FadeAnimatedTextExampleState();
+}
+
+class _FadeAnimatedTextExampleState extends State<FadeAnimatedTextExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const SizedBox(
+                  width: 20.0,
+                  height: 100.0,
+                ),
+                Text(
+                  'Fade: ',
+                  style: GoogleFonts.adamina(
+                    fontSize: 20.0,
+                    color: Colors.lightBlue,
+                  ),
+                ),
+                const SizedBox(
+                  width: 20.0,
+                  height: 100.0,
+                ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    FadeAnimatedText(
+                      'NOW!',
+                      textStyle: GoogleFonts.aclonica(
+                        color: Colors.green,
+                        fontSize: 60.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    FadeAnimatedText(
+                      'AT ONCE!!',
+                      textStyle: GoogleFonts.cairo(
+                        color: Colors.red,
+                        fontSize: 70.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    FadeAnimatedText(
+                      'OR NEVER!!!',
+                      textStyle: GoogleFonts.mandali(
+                        color: Colors.yellow,
+                        fontSize: 80.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
