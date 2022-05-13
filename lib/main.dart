4a615449c135c820e7a2ee343e962f143f8facc2
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-13 06:31:16 +0530

    Just before final

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,6 @@ void main() => runApp(const AnimatedTextExampleApp());
 /* 
   
       AnimatedTextExample(
-        label: 'Flicker',
-        color: Colors.pink[300],
-        child: DefaultTextStyle(
-          style: const TextStyle(
-            fontSize: 35,
-            color: Colors.white,
-            shadows: [
-              Shadow(
-                blurRadius: 7.0,
-                color: Colors.white,
-                offset: Offset(0, 0),
-              ),
-            ],
-          ),
-          child: AnimatedTextKit(
-            repeatForever: true,
-            animatedTexts: [
-              FlickerAnimatedText('Flicker Frenzy'),
-              FlickerAnimatedText('Night Vibes On'),
-              FlickerAnimatedText("C'est La Vie !"),
-            ],
-            onTap: onTap,
-          ),
-        ),
-      ),
-      AnimatedTextExample(
         label: 'Combination',
         color: Colors.pink,
         child: AnimatedTextKit(
